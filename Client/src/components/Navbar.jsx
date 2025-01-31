3ba4ea1e826256b32e082a36ebb33ab05a44219f
Brayan Velasco <bvelasco10b@gmail.com>
2025-01-31 00:35:41 -0500

    fix: Corregido error tipográfico en el componente Navbar: 'EVenttum' -> 'Eventtum'

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
                     sx={{ flexGrow: 1, cursor: 'pointer' }} 
                     onClick={() => navigate("/")}
                 >
-                    EVenttum Maker
+                    Eventtum Maker
                 </Typography>
                 {isAuthenticated ? (
                     <>
